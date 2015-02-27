5d5c049212a1bdec4b5a95bf0e56266b0f579a5b
Petemir <matiaslaporte@gmail.com>
2015-02-26 21:19:34 -0300

    Mas o menos listo el delay para ASM con float...

--- src/delay.asm
+++ src/delay.asm
@@ -18,8 +18,14 @@ section .text
     push r14
     push r15
 
-    cmp r9, 2
-    je cycle_stereo
+    cmp r9, 1
+    je cycle_mono
+
+    mov eax,  0x3F000000 ; 0.5 en eax
+    movd xmm2, eax
+    shufps xmm2, xmm2, 0x00 ; xmm2 = 0.5 | 0.5 | 0.5 | 0.5 |
+
+    jmp cycle_stereo
 
     cycle_mono:
     cmp rcx, 0  ; ya recorrí todo el buffer?
@@ -30,25 +36,23 @@ section .text
     ; dataBuffOut[i] = dataBuffIn[i]
     ; dataBuffOut[i+1] = dataBuffEffect[i]
 
-    movups xmm0, [rdi]  ; xmm0 = dataBuffIn[0..3]
-    movups xmm1, [rdx]  ; xmm1 = dataBuffEffect[0..3]
+    movaps xmm0, [rdi]  ; xmm0 = dataBuffIn[0..3]
+    movaps xmm1, [rdx]  ; xmm1 = dataBuffEffect[0..3]
 
-    movups xmm2, xmm0   ; trabajo en xmm2 para no ensuciar xmm0
-    punpckldq xmm2, xmm1; mezclo en xmm2 las partes altas de xmm2 y xmm1
-    movups [rsi], xmm2  ; dataBuffOut = xmm2 = dataBuffIn[0]+dataBuffEffect[0]+dataBuffIn[1]+dataBuffEffect[1]
+    movaps xmm2, xmm0   ; trabajo en xmm2 para no ensuciar xmm0
+    punpckldq xmm2, xmm1; mezclo en xmm2 las partes bajas de xmm2 y xmm1
+    movaps [rsi], xmm2  ; dataBuffOut = xmm2 = dataBuffIn[0]+dataBuffEffect[0]+dataBuffIn[1]+dataBuffEffect[1]
     add rsi, 16         ; me muevo 16 bytes (4 floats) en el buffer de salida
-    movups xmm2, xmm0
-    punpckhdq xmm2, xmm1; mezclo en xmm2 las partes bajas de xmm2 y xmm1
-    movups [rsi], xmm2  ; dataBuffOut = xmm2 = dataBuffIn[2]+dataBuffEffect[2]+dataBuffIn[3]+dataBuffEffect[3]
+    movaps xmm2, xmm0
+    punpckhdq xmm2, xmm1; mezclo en xmm2 las partes altas de xmm2 y xmm1
+    movaps [rsi], xmm2  ; dataBuffOut = xmm2 = dataBuffIn[2]+dataBuffEffect[2]+dataBuffIn[3]+dataBuffEffect[3]
     add rsi, 16
 
     ; dataBuffEffect[i] = dataBuffIn[i] * decay
-
-    movss xmm0, [r8]        ; xmm0 = decay | - | - | - |
-    shufps xmm0, xmm0, 0x00 ; xmm0 = decay | decay | decay | decay |
-    movaps xmm1, [rdi]     ; xmm1 = dataBuffIn
-    mulps xmm1, xmm0       ; xmm1 = dataBuffIn * decay
-    movaps [rdx], xmm1     ; dataBuffEffect = xmm1
+    movss xmm1, [r8]        ; xmm1 = decay | - | - | - |
+    shufps xmm1, xmm1, 0x00 ; xmm1 = decay | decay | decay | decay |
+    mulps xmm0, xmm1       ; xmm0 = dataBuffIn * decay
+    movaps [rdx], xmm0     ; dataBuffEffect = xmm0
 
     sub rcx,4    ; se procesaron 4 frames
     add rdi,16   ; nos movemos 16 bytes (4 float) en cada puntero
@@ -86,76 +90,79 @@ section .text
     cycle_stereo:
     cmp rcx, 0  ; ya recorrí todo el buffer?
     je fin
-    cmp rcx, 1  ; caso borde: cantidad de frames impar
-    je odd_frames_mono
-;    cmp rcx, 4  ; caso borde: cantidad de frames impar
-;    jl odd_frames_mono
+    cmp rcx, 2  ; caso borde: cantidad de frames restantes igual a 2 (siempre es par, por ser stereo)
+    je remaining_frames_stereo
 
-    ; HACER PROMEDIO DE IZQ+DER EN CASO DE MONO
-    ; dataBuffOut[i] = dataBuffIn[i] + dataBuffEffect
-;double
-    movapd xmm0, [rdi]  ; xmm0 = dataBuffIn
-    movapd xmm1, [rdx]  ; xmm1 = dataBuffEffect
-    addpd xmm0, [rdx]   ; xmm0 = dataBuffIn + dataBuffEffect
-    movapd [rsi], xmm0  ; dataBuffOut = xmm0
-;float
-    ;movaps xmm0, [rdi]  ; xmm0 = dataBuffIn
-    ;movaps xmm1, [rdx]  ; xmm1 = dataBuffEffect
-    ;addps xmm0, [rdx]   ; xmm0 = dataBuffIn + dataBuffEffect
-    ;movaps [rsi], xmm0  ; dataBuffOut = xmm0
+    ; dataBuffOut[i] = 0.5*(dataBuffIn[i]+dataBuffIn[i+1])
+    movaps xmm0, [rdi]  ; xmm0 = dataBuffIn[0] | dataBuffIn[1] | dataBuffIn[2] | dataBuffIn[3] |
+    mulps xmm0, xmm2    ; xmm0 = 0.5*dataBuffIn[0..3]
+
+    movaps xmm3, xmm0   ; xmm3 = xmm0
+    shufps xmm3, xmm3, 0x70   ; xmm3 = 0.5*dataBuffIn[1] | 0.5*dataBuffIn[3] | .. | .. |
+    shufps xmm0, xmm0, 0x20   ; xmm0 = 0.5*dataBuffIn[0] | 0.5*dataBuffIn[2] | .. | .. |
+    addps xmm0, xmm3          ; xmm0 = 0.5*(dataBuffIn[0+1]) | 0.5*(dataBuffIn[2+3])
+
+    ; dataBuffOut[i+1] = 0.5*(dataBuffEffect[i]+dataBuffEffect[i+1])
+    movaps xmm1, [rdx]  ; xmm1 = dataBuffEffect[0..3]
+    mulps xmm1, xmm2    ; xmm1 = 0.5*dataBuffEffect[0..3]
+    
+    movaps xmm3, xmm1
+    shufps xmm3, xmm3, 0x70   ; xmm3 = dataBuffEffect[1] | dataBuffEffect[3] | .. | .. |
+    shufps xmm1, xmm1, 0x20   ; xmm1 = dataBuffEffect[0] | dataBuffEffect[2] | .. | .. |
+    addps xmm1, xmm3          ; xmm1 = 0.5*(dataBuffEffect[0+1]) | 0.5*(dataBuffEffect[2+3])
+
+    punpckhdq xmm0, xmm1      ; xmm0 = 0.5*(dataBuffIn[0+1]) | 0.5*(dataBuffEffect[0+1]) | 0.5*(dataBuffIn[2+3]) | 0.5*(dataBuffEffect[2+3])
+    movaps [rsi], xmm0
 
     ; dataBuffEffect[i] = dataBuffIn[i] * decay
-;double
-    movhpd xmm0, [r8]  ; xmm0 = decay | -
-    movlpd xmm0, [r8]  ; xmm0 = decay | decay
-    movapd xmm1, [rdi] ; xmm1 = dataBuffIn
-    mulpd xmm1, xmm0   ; xmm1 = dataBuffIn * decay
-    movapd [rdx], xmm1    ; dataBuffEffect = xmm1
-;float
-    ;movhps xmm0, [r8]  ; xmm0 = decay | -
-    ;movlps xmm0, [r8]  ; xmm0 = decay | decay
-    ;movaps xmm1, [rdi] ; xmm1 = dataBuffIn
-    ;mulps xmm1, xmm0   ; xmm1 = dataBuffIn * decay
-    ;movaps [rdx], xmm1    ; dataBuffEffect = xmm1
+    movss xmm1, [r8]        ; xmm1 = decay | - | - | - |
+    shufps xmm1, xmm1, 0x00 ; xmm1 = decay | decay | decay | decay |
+    movaps xmm0, [rdi]     ; xmm0 = dataBuffIn
+    mulps xmm0, xmm1       ; xmm0 = dataBuffIn[0..3] * decay
+    movaps [rdx], xmm0     ; dataBuffEffect = xmm0
 
-    sub rcx,2   ; se procesaron 2 frames
-    add rdi,16  ; nos movemos 16 bytes (2 doubles) en cada puntero
-;    sub rcx,4   ; se procesaron 4 frames
-;    add rdi,16   ; nos movemos 16 bytes (4 float) en cada puntero
+    sub rcx,4   ; se procesaron 4 frames
+    add rdi,16   ; nos movemos 16 bytes (4 float) en cada puntero
     add rsi,16
     add rdx,16
 
     jmp cycle_stereo
 
-    odd_frames_stereo:
-    ; frame restante, procesamos un sólo double
-    ; dataBuffOut[i] = dataBuffIn[i] + dataBuffEffect
-;double
-    movlpd xmm0, [rdi]  ; xmm0 = dataBuffIn
-    movlpd xmm1, [rdx]  ; xmm1 = dataBuffEffect
-    addsd xmm0, [rdx]   ; xmm0 = dataBuffIn + dataBuffEffect
-    movlpd [rsi], xmm0  ; dataBuffOut = xmm0
-;float
-    ;movlps xmm0, [rdi]  ; xmm0 = dataBuffIn
-    ;movlps xmm1, [rdx]  ; xmm1 = dataBuffEffect
-    ;addss xmm0, [rdx]   ; xmm0 = dataBuffIn + dataBuffEffect
-    ;movlps [rsi], xmm0  ; dataBuffOut = xmm0
+    remaining_frames_stereo:
+    ; frames restantes, procesamos dos floats (canal izquierdo y derecho)
+    ; dataBuffOut[i] = 0.5*(dataBuffIn[i]+dataBuffIn[i+1])
+    
+    movlps xmm0, [rdi]  ; xmm0 = dataBuffIn[0]+dataBuffIn[1]
+    mulps xmm0, xmm2    ; xmm0 = 0.5*dataBuffIn[0,1]
+
+    movaps xmm3, xmm0
+    shufps xmm3, xmm3, 0x10 ; xmm3 = dataBuffIn[1] | .. | .. | .. |
+    addss xmm0, xmm3    ; xmm0 = 0.5*(dataBuffIn[0+1]) | .. | .. | .. |
+
+    movlps [rsi], xmm0
+    add rsi, 4
+
+    ; dataBuffOut[i+1] = 0.5*(dataBuffEffect[i]+dataBuffEffect[i+1])
+    movlps xmm1, [rdx]  ; xmm1 = dataBuffEffect[0]+dataBuffEffect[1]
+    mulps xmm1, xmm2    ; xmm1= 0.5*dataBuffEffect[0,1]
+    
+    movaps xmm3, xmm1
+    shufps xmm3, xmm3, 0x10 ; xmm3 = dataBuffEffect[1] | .. | .. | .. |
+    addss xmm1, xmm3    ; xmm0 = 0.5*(dataBuffEffect[0+1]) | .. | .. | .. |
+
+    movlps [rsi], xmm1
+    add rsi, 4
 
     ; dataBuffEffect[i] = dataBuffIn[i] * decay
-;double
-    movlpd xmm0, [r8]  ; xmm0 = decay | -
-    movlpd xmm1, [rdi] ; xmm1 = dataBuffIn
-    mulsd xmm1, xmm0   ; xmm1 = dataBuffIn * decay
-    movlpd [rdx], xmm1    ; dataBuffEffect = xmm1
-;float
-    ;movlps xmm0, [r8]  ; xmm0 = decay | -
-    ;movlps xmm1, [rdi] ; xmm1 = dataBuffIn
-    ;mulss xmm1, xmm0   ; xmm1 = dataBuffIn * decay
-    ;movlps [rdx], xmm1    ; dataBuffEffect = xmm1
+    movss xmm0, [r8]        ; xmm0 = decay | - | - | - |
+    shufps xmm0, xmm0, 0x00 ; xmm0 = decay | decay | decay | decay |
+    movlps xmm1, [rdi]     ; xmm1 = dataBuffIn[0] | dataBuffIn[1] | .. | .. |
+    mulps xmm1, xmm0       ; xmm1 = dataBuffIn[0] * decay | dataBuffIn[1] * decay | .. | .. |
+    movlps [rdx], xmm1     ; dataBuffEffect = xmm1
 
-;    sub rcx,1
-;    cmp rcx, 0
-;    jne odd_frames_stereo
+    add rdi, 8
+    add rdx, 8
+    sub rcx, 2
 
     fin:
     pop r15
--- src/effects.c
+++ src/effects.c
@@ -242,9 +242,10 @@ void delay_c(float delayInSec, float decay) {
             // Original por el canal izquierdo, efecto por el derecho; 
             if (inFileStr.channels == 2)
             {
-                dataBuffOut[out_i++] = 0.5 * (dataBuffIn[i] + dataBuffIn[i+1]);
-                dataBuffOut[out_i++] = 0.5 * (dataBuffEffect[i] + dataBuffEffect[i+1]);
+                dataBuffOut[out_i++] = 0.5 * dataBuffIn[i] + 0.5 * dataBuffIn[i+1];
+                dataBuffOut[out_i++] = 0.5 * dataBuffEffect[i] + 0.5 * dataBuffEffect[i+1];
 
+                dataBuffEffect[i] = dataBuffIn[i] * decay;
                 i++;    // Avanzo de a dos en dataBuffIn
             } else {
                 dataBuffOut[out_i++] = dataBuffIn[i];
@@ -274,71 +275,6 @@ void delay_c(float delayInSec, float decay) {
     // [/Limpieza]
 }
 
-// Double
-// void delay_c(double delayInSec, double decay) {
-//     unsigned int delayInFrames = ceil(delayInSec*inFileStr.samplerate);  // Frames de delay
-//     unsigned int bufferFrameSize = delayInFrames*inFileStr.channels;  // Tamaño del buffer en frames
-//     unsigned int bufferSize = bufferFrameSize*sizeof(double);       // Tamaño del buffer en bytes
-//     unsigned int bufferFrameSizeOut = delayInFrames*2;
-//     unsigned int bufferSizeOut = bufferFrameSize*bufferFrameSizeOut;
-
-//     dataBuffIn = (double*)malloc(bufferSize);
-//     dataBuffOut = (double*)malloc(bufferSizeOut);
-//     double *dataBuffEffect = (double*)malloc(bufferSize);
-//     // Limpio buffers
-//     clean_buffer(dataBuffIn, bufferFrameSize);
-//     clean_buffer(dataBuffEffect, bufferFrameSize);
-//     clean_buffer(dataBuffOut, bufferFrameSizeOut);
-
-//     unsigned long int start, end, cantCiclos = 0;
-//     // [Lecto-escritura de datos]
-//     while ((framesRead = sf_readf_double(inFilePtr, dataBuffIn, delayInFrames))) {
-//         MEDIR_TIEMPO_START(start);
-//         for (unsigned int i = 0, out_i = 0; i < bufferFrameSize; i++) {
-            
-//             // Original por el canal izquierdo, efecto por el derecho; 
-            
-//             if (inFileStr.channels == 2)
-//             {
-//                 dataBuffOut[out_i++] = 0.5 * (dataBuffIn[i] + dataBuffIn[i+1]);
-//                 dataBuffOut[out_i++] = 0.5 * (dataBuffEffect[i] + dataBuffEffect[i+1]);
-
-//                 dataBuffOut[out_i++] = dataBuffIn[i]
-//                 i++;
-//             } else {
-//                 dataBuffOut[out_i++] = dataBuffIn[i];
-//                 dataBuffOut[out_i++] = dataBuffEffect[i];
-//             }
-
-//             if (debug) {
-//                 dataBuffOut[i] = dataBuffEffect[i];
-//             } else {
-//                 dataBuffOut[i] = dataBuffIn[i] + dataBuffEffect[i];
-//             }
-
-//             dataBuffEffect[i] = dataBuffIn[i] * decay;  // Delay simple
-//         }
-//         MEDIR_TIEMPO_STOP(end);
-//         cantCiclos += end-start;
-
-//         framesWritten = sf_write_double(outFilePtr, dataBuffOut, framesRead*2);
-//         sf_write_sync(outFilePtr);
-//     }
-//     // [/Lecto-escritura de datos]
-//     printf("\tTiempo de ejecución:\n");
-//     printf("\t  Comienzo                          : %lu\n", start);
-//     printf("\t  Fin                               : %lu\n", end);
-//     // printf("\t  # iteraciones                     : %d\n", cant_iteraciones);
-//     printf("\t  # de ciclos insumidos totales     : %lu\n", cantCiclos);
-//     // printf("\t  # de ciclos insumidos por llamada : %.3f\n", (float)cantCiclos/(float)cant_iteraciones);
- 
-//     // [Limpieza]
-//     free(dataBuffIn);
-//     free(dataBuffOut);
-//     free(dataBuffEffect);
-//     // [/Limpieza]
-// }
-
 // Float
 void delay_asm_caller(float delayInSec, float decay) {
     unsigned int delayInFrames = ceil(delayInSec*inFileStr.samplerate);  // Frames de delay
@@ -366,10 +302,9 @@ void delay_asm_caller(float delayInSec, float decay) {
             delay_asm(dataBuffIn, dataBuffOut, dataBuffEffect, framesRead*inFileStr.channels, &decay, inFileStr.channels);
             MEDIR_TIEMPO_STOP(end);
         }
-        cantCiclos+= end-start;
+        cantCiclos += end-start;
 
         framesWritten = sf_write_float(outFilePtr, dataBuffOut, framesRead*outFileStr.channels);
-        printf("%d.", framesWritten);
         sf_write_sync(outFilePtr);
     }
 
@@ -384,49 +319,3 @@ void delay_asm_caller(float delayInSec, float decay) {
     free(dataBuffOut);
     free(dataBuffEffect);
 }
-
-// Double
-/*void delay_asm_caller(double delayInSec, double decay) {
-    unsigned int delayInFrames = ceil(delayInSec*inFileStr.samplerate);  // Frames de delay
-    unsigned int bufferFrameSize = delayInFrames*inFileStr.channels;  // Tamaño del buffer en frames
-    unsigned int bufferSize = bufferFrameSize*sizeof(double);       // Tamaño del buffer en bytes
-    unsigned int bufferFrameSizeOut = delayInFrames*2;
-    unsigned int bufferSizeOut = bufferFrameSize*bufferFrameSizeOut;
-
-    dataBuffIn = (double*)malloc(bufferSize);
-    dataBuffOut = (double*)malloc(bufferSizeOut);
-    double *dataBuffEffect = (double*)malloc(bufferSize);
-    // Limpio buffers
-    clean_buffer(dataBuffIn, bufferFrameSize);
-    clean_buffer(dataBuffEffect, bufferFrameSize);
-    clean_buffer(dataBuffOut, bufferFrameSizeOut);
-
-    unsigned long int start, end = 0;
-    while ((framesRead = sf_readf_double(inFilePtr, dataBuffIn, delayInFrames))) {
-        if (debug) {
-            MEDIR_TIEMPO_START(start);
-        //    delay_debug_asm(dataBuffIn, dataBuffOut, dataBuffEffect, framesRead*inFileStr.channels, &decay, inFileStr.channels);
-            MEDIR_TIEMPO_STOP(end);
-        } else {
-            MEDIR_TIEMPO_START(start);
-            delay_asm(dataBuffIn, dataBuffOut, dataBuffEffect, framesRead*inFileStr.channels, &decay, inFileStr.channels);
-            MEDIR_TIEMPO_STOP(end);
-        }
-        cantCiclos+= end-start;
-
-        framesWritten = sf_write_double(outFilePtr, dataBuffOut, framesRead*inFileStr.channels);
-        sf_write_sync(outFilePtr);
-    }
-
-    printf("\tTiempo de ejecución:\n");
-    printf("\t  Comienzo                          : %lu\n", start);
-    printf("\t  Fin                               : %lu\n", end);
-    // printf("\t  # iteraciones                     : %d\n", cant_iteraciones);
-    printf("\t  # de ciclos insumidos totales     : %lu\n", cantCiclos);
-    // printf("\t  # de ciclos insumidos por llamada : %.3f\n", (float)cantCiclos/(float)cant_iteraciones);
-
-    free(dataBuffIn);
-    free(dataBuffOut);
-    free(dataBuffEffect);
-}
-*/
